e1ebd8a38c794cde23401588a908afa20e264031
Darius Drachenfels Wiatrak <dariusz.wiatrak@gmail.com>
2017-12-27 17:26:46 +0000

    Add pattern that can define what is expected from url to conform.
    This way we can specify that we have versions, namespaces or anything
    else that is not exactly part of url.

--- tests/test_spliting.py
+++ tests/test_spliting.py
@@ -28,3 +28,75 @@ class TestSplit(unittest.TestCase):
         """
         with self.assertRaises(url4api.exceptions.UnrecognisedProtocol):
             url4api.split('ftp://example.com:30/3.0/product/list?boom=1')
+
+    def test_more_complex_splitting(self):
+        """Name: TestSplit.test_more_complex_splitting
+        """
+        processed = url4api.split(
+            'https://example.com/3.0/food_items/product/uid/1/?boom=1',
+            pattern='<version:number>/<namespace>/<...>')
+
+        self.assertEqual(processed.url, 'product/uid/1/?boom=1')
+        self.assertEqual(processed.domain, 'example.com')
+        self.assertEqual(processed.port, None)
+
+        self.assertEqual(processed.version, 3)
+        self.assertEqual(processed.namespace, 'food_items')
+
+    def test_hard_fixed_url(self):
+        """Name: TestSplit.test_hard_fixed_url_
+
+        Use case when we want to limit number of arguments on url.
+        """
+        processed = url4api.split(
+            'https://example.com/animal/lion/', pattern='/<key>/<value>/')
+
+        self.assertEqual(processed.url, '')
+        self.assertEqual(processed.domain, 'example.com')
+        self.assertEqual(processed.port, None)
+
+        self.assertEqual(processed.key, 'animal')
+        self.assertEqual(processed.value, 'lion')
+
+        processed = url4api.split(
+            'https://example.com/animal/lion/', pattern='<key>/<value>/')
+
+        self.assertEqual(processed.url, '')
+        self.assertEqual(processed.domain, 'example.com')
+        self.assertEqual(processed.port, None)
+
+        self.assertEqual(processed.key, 'animal')
+        self.assertEqual(processed.value, 'lion')
+
+        processed = url4api.split(
+            'https://example.com/animal/lion/', pattern='<key>/<value>')
+
+        self.assertEqual(processed.url, '')
+        self.assertEqual(processed.domain, 'example.com')
+        self.assertEqual(processed.port, None)
+
+        self.assertEqual(processed.key, 'animal')
+        self.assertEqual(processed.value, 'lion')
+
+    def test_hard_fixed_url_with_superflous_elements(self):
+        """Name: TestSplit.test_hard_fixed_url_with_superflous_elements
+        """
+        processed = url4api.split(
+            'https://example.com/animal_feed/lion/meat/potato/ants/',
+            pattern='/<key>/<value>/<value1>')
+
+        self.assertEqual(processed.url, '')
+        self.assertEqual(processed.domain, 'example.com')
+        self.assertEqual(processed.port, None)
+
+        self.assertEqual(processed.key, 'animal_feed')
+        self.assertEqual(processed.value, 'lion')
+        self.assertEqual(processed.value1, 'meat')
+
+    def test_argparse_in_wrong_place(self):
+        """Name: TestSplit.test_argparse_in_wrong_place
+        """
+        with self.assertRaises(url4api.exceptions.InvalidInputPattern):
+            processed = url4api.split(
+                'https://example.com/animal_feed/lion/meat/potato/ants/',
+                pattern='/<key>/<...>/<value>/<value1>')
--- url4api/main.py
+++ url4api/main.py
@@ -2,47 +2,129 @@
 parsing/processing.
 """
 
+import re
+
 from urllib.parse import urlparse
 
 from . import exceptions, models
 
 
-def split(url):
-    """Function takes a url path and extracts from it version and base part.
+def parse_pattern(pattern):
+    cast_types = {
+        'number': lambda val: int(float(val)),
+        'double': float,
+        'string': str,
+        'bool': bool,
+    }
+
+    if not pattern:
+        raise exceptions.InvalidInputPattern("Pattern cannot be empty")
+
+    elements = re.findall(r'<([\w\.]+)(:(\w+))?>', pattern)
+
+    groups = []
+
+    for (element, separator, item_type) in elements:
+        if not item_type:
+            item_type = 'string'
+
+        if item_type not in cast_types:
+            raise exceptions.InvalidInputPattern(
+                "There is unrecognised item type ({}) for element {}".format(
+                    item_type, element))
+
+        if (element, item_type) in groups:
+            raise exceptions.InvalidInputPattern(
+                "Element ({}) present twice on a pattern".format(element))
+
+        del separator
+
+        groups.append((element, cast_types[item_type]))
+
+    return groups
+
+
+def split(url, pattern=None):
+    """Function takes a string and parses it into restful api relevant chunks.
+
+    Second argument is optional. It allows to predefine that given url has to
+    have certain structure. For example we can enforce that version or
+    namespace is present. If not provided it will default to:
+
+        <version:double>/<rest_of_url>
+
+    Pattern consists of backslash separated groups. Each group member needs to
+    be defined as:
+
+        <name>
+
+    or
+
+        <name:type>
+
+    Where type can be number, double, string or bool (default: string).
     """
+    if pattern is None:
+        pattern = '/<version:double>/<...>'
+
     parsed_url = urlparse(url)
 
     if parsed_url.scheme not in ('http', 'https'):
         raise exceptions.UnrecognisedProtocol(
             "Only http or https is ok with us")
 
-    parts = parsed_url.path.split('/')
+    groups = parse_pattern(pattern)
 
-    try:
-        version = float(parts[1])
-    except (NameError, TypeError, ValueError):
-        raise exceptions.InvalidUrlPattern(
-            "Version has to be a float number.")
+    url = parsed_url.path
 
-    try:
-        base_url = str("/".join(parts[2:]))
-    except (TypeError, exceptions.InvalidUrlPattern):
-        raise exceptions.BadRouting(
-            "Path is corrupted, dont know what to do with it")
+    if url.startswith('/'):
+        url = url[1:]
+
+    kwargs = {}
+
+    while url:
+        if not groups:
+            break
+
+        name, cast_to = groups.pop(0)
+
+        idx = url.find('/')
+
+        if name == '...':
+            if groups:
+                raise exceptions.InvalidInputPattern(
+                    "Argpars <...> cannot be followed by any other group")
+
+            value = url
+            url = ''
+            name = 'url'
+
+        else:
+            value = url[:idx]
+            url = url[idx + 1:]
+
+            try:
+                value = cast_to(value)
+            except (NameError, TypeError, ValueError):
+                raise exceptions.InvalidUrlPattern(
+                    "Unable to cast value ({}) using ({})".format(
+                        value, cast_to))
+
+        kwargs[name] = value
 
     if parsed_url.params:
-        base_url += ';' + parsed_url.params
+        kwargs['url'] += ';' + parsed_url.params
 
     elif parsed_url.query:
-        base_url += '?' + parsed_url.query
+        kwargs['url'] += '?' + parsed_url.query
 
     elif parsed_url.fragment:
-        base_url += '#' + parsed_url.fragment
+        kwargs['url'] += '#' + parsed_url.fragment
 
     return models.ApiUrl(
-        url=base_url,
-        version=version,
         protocol=parsed_url.scheme,
         domain=parsed_url.hostname,
-        port=parsed_url.port
+        port=parsed_url.port,
+        url=kwargs.pop('url') if 'url' in kwargs else '',
+        **kwargs
     )
