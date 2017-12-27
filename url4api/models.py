4c1a21c25b67db6cd3987fcfb9381597ecafc2ba
Darius Drachenfels Wiatrak <dariusz.wiatrak@gmail.com>
2017-12-27 17:26:15 +0000

    Api url can handle any number of additional arguments.

--- url4api/models.py
+++ url4api/models.py
@@ -1,16 +1,23 @@
 class ApiUrl(object):
-    def __init__(self, version, url, protocol, port, domain):
-        self.version = version
-        self.url = url
+    def __init__(self, protocol, port, domain, url, **kwargs):
         self.protocol = protocol
-        self.domain = domain
         self.port = port
+        self.domain = domain
+        self.url = url
+        self.kwargs = kwargs
+
+    def __getattr__(self, key):
+        return self.kwargs[key]
 
     def as_dict(self):
-        return {
-            'version': self.version,
-            'url': self.url,
+        return_val = {
             'protocol': self.protocol,
             'domain': self.domain,
             'port': self.port,
+            'url': self.url,
         }
+
+        for key, value in self.kwargs.items():
+            return_val[key] = value
+
+        return return_val
