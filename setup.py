0dc2968ab0d2cb6bad8df1427ddf5e216381e764
Darius Drachenfels Wiatrak <dariusz.wiatrak@gmail.com>
2018-03-12 16:15:11 +0000

    Integrage tests + add long description for a package.

--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
 from setuptools import setup
 
 
+def readme():
+    with open('README.rst') as fpl:
+        return fpl.read()
+
+
 setup(
     name='url2vapi',
     version='1.0',
     description=(
         'Tools extracts constant elements from url (for example version)'
     ),
+    long_description=readme(),
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    url='https://github.com/Drachenfels/url2vapi',
     author="Drachenfels",
     author_email="drachu@gmail.com",
     license="MIT",
     packages=['url2vapi'],
-    zip_safe=False)
+    zip_safe=False
+)
